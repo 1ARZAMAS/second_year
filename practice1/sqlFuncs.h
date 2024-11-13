91301da23521ad8203b707b38a6cc304b98173f4
Елизавета <131687570+1ARZAMAS@users.noreply.github.com>
2024-11-13 14:28:45 +0700

    Update sqlFuncs.h

--- practice1/sqlFuncs.h
+++ practice1/sqlFuncs.h
@@ -6,3 +6,5 @@ void insertFunc(const DatabaseManager& dbManager, const std::string& tableName, 
 void deleteFunc(const DatabaseManager& dbManager, const std::string& tableName, std::string& query, LinkedList& tableFromQuery, LinkedList& columnFromQuery);
 
 void crossJoin(int& fileCountFirstTable, int& fileCountSecondTable, const DatabaseManager& dbManager, const std::string& tableName, LinkedList& columnsFromQuery);
+
+void selectWithWhere(int& fileCountFirstTable, int& fileCountSecondTable, const DatabaseManager& dbManager, const std::string& query, LinkedList& tablesFromQuery, LinkedList& columnsFromQuery);
