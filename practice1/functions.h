7cc2a1312163844e500d5ac396b7883b1c925035
Елизавета <131687570+1ARZAMAS@users.noreply.github.com>
2024-11-13 14:27:57 +0700

    Update functions.h

--- practice1/functions.h
+++ practice1/functions.h
@@ -12,3 +12,7 @@ int amountOfCSV(const DatabaseManager& dbManager, const std::string& tableName);
 void copyFirstRow(std::string& firstTable, std::string& tableDir);
 
 bool findDot(std::string str);
+
+std::string getColumnValue(int& fileCountFirstTable, int& fileCountSecondTable, LinkedList& tablesFromQuery, LinkedList& columnsFromQuery, const std::string& columnName, int rowIndex1, int rowIndex2, const DatabaseManager& dbManager);
+
+bool recursionFunc(int& fileCountFirstTable, int& fileCountSecondTable, const std::string& query, LinkedList& tablesFromQuery, LinkedList& columnsFromQuery, int rowIndex1, int rowIndex2, const DatabaseManager& dbManager);
