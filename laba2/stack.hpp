3194bcd9bf2350e78b14880b7a958a004622e13f
Елизавета <131687570+1ARZAMAS@users.noreply.github.com>
2024-11-15 23:02:39 +0700

    Update stack.hpp

--- laba2/stack.hpp
+++ laba2/stack.hpp
@@ -1,60 +1,65 @@
-#include "header.h"
+#pragma once
+#include <iostream>
 
+using namespace std;
+
+template <typename T>
 struct StackNode {
-    std::string data;
+    T data;
     StackNode* next;
 
-    StackNode(std::string value) : data(value), next(nullptr) {}
+    StackNode(T value) : data(value), next(nullptr) {}
 };
 
+template <typename T>
 struct Stack {
-    StackNode* top;
+    StackNode<T>* top;
     Stack(){
         top = nullptr;
-        loadFromFile("stack.data");
     }
-    
     void clear();
-    void push(std::string value); //добавление
+    void push(T value); //добавление
     void pop(); //удаление
     void display(); //вывод стека
-    void saveToFile(const std::string& filename);
-    void loadFromFile(const std::string& filename);
+    bool empty();
+    T Top();
 };
 
-void Stack::clear(){
+template <typename T>
+void Stack<T>::clear(){
     while (top != nullptr) {
         pop();
     }
 }
 
-void Stack::push(std::string value){
-    StackNode* node = new StackNode(value);
+template <typename T>
+void Stack<T>::push(T value){
+    StackNode<T>* node = new StackNode(value);
     if (top == nullptr){ // если стек пустой
         top = node; // то top = введеное значение
     } else {
         node->next = top; // если стек не пустой, то тогда предыдущая top 
         top = node; // станет 2м значением, новое значение = top
     }
-    saveToFile("stack.data"); // Сохраняем состояние стека после добавления
 }
 
-void Stack::pop(){
+template <typename T>
+void Stack<T>::pop(){
     if (top == nullptr){
         cout << "Stack is empty, value cannot be deleted" << endl;
     } else {
-        StackNode* temp = top; // новая переменная, которая указывает на топ
+        StackNode<T>* temp = top; // новая переменная, которая указывает на топ
         top = top->next; // перезаписываем
         delete temp; // и удаляем перменную
-        saveToFile("stack.data"); // Сохраняем состояние стека после добавления
     }
 }
-    
-void Stack::display(){
+
+template <typename T>
+void Stack<T>::display(){
     if (top == nullptr){
         cout << "Stack is empty" << endl;
     } else {
-        StackNode* current = top;
+        StackNode<T>* current = top;
         while(current != nullptr){
             cout << current->data << " ";
             current = current->next;
@@ -63,31 +68,19 @@ void Stack::display(){
     }
 }
 
-void Stack::saveToFile(const std::string& filename) {
-    std::ofstream file(filename);
-    if (!file) {
-        cout << "Cannot open file for writing: " << filename << endl;
-        return;
+template <typename T>
+bool Stack<T>::empty(){
+    if (top == nullptr){
+        return true;
+    } else {
+        return false;
     }
-    StackNode* current = top;
-    while (current != nullptr) {
-        file << current->data << endl;
-        current = current->next;
-    }
-    file.close();
 }
 
-// Загрузка стека из файла
-void Stack::loadFromFile(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file) {
-        cout << "Cannot open file for reading: " << filename << endl;
-        return;
+template <typename T>
+T Stack<T>::Top() {
+    if (this->top == nullptr) {
+        throw std::runtime_error("Stack is empty!");
     }
-    std::string value;
-    clear();  // Сбрасываем текущий стек перед загрузкой
-    while (std::getline(file, value)) {
-        push(value);
-    }
-    file.close();
+    return this->top->data;
 }
