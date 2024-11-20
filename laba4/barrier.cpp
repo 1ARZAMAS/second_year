e3b01ed972c2ef7bfb2f40c8249960c969862e74
Елизавета <131687570+1ARZAMAS@users.noreply.github.com>
2024-11-20 17:49:20 +0700

    Add files via upload

--- laba4/barrier.cpp
+++ laba4/barrier.cpp
@@ -10,3 +10,52 @@
 подождать достижения барьера всеми потоками (процессами) группы. 
 Когда все потоки (процессы) достигли барьера, их выполнение продолжается
 */
+
+#include <iostream>
+#include <random>
+#include <string>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <barrier>
+
+using namespace std;
+using namespace std::chrono;
+
+mutex Mutex;
+
+void randomChars(int thread_id, int letter_count) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(32, 126);  // ASCII диапазон видимых символов
+    string chars;
+    for (int i = 0; i < letter_count; ++i){
+        char letter = static_cast<char>(dis(gen));
+        chars += letter;
+    }
+    cout << "Thread " << thread_id << ": " << chars << endl; 
+}
+
+void worker(int id, barrier<>& barrier, int letter_count){
+    barrier.arrive_and_wait();
+    lock_guard<std::mutex> lock(Mutex);
+    randomChars(id, letter_count);// Генерация случайных символов
+}
+
+int main(){
+    vector<thread> threads;
+    barrier<> barrier(4);
+    auto start = high_resolution_clock::now(); // начало таймера
+    for (int i = 0; i < 4; ++i) { // Запуск потоков
+        threads.push_back(thread(worker, i, ref(barrier), 10));
+    }
+
+    // Ожидание завершения всех потоков
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    auto end = high_resolution_clock::now(); // конец таймера
+    auto duration = duration_cast<microseconds>(end - start);
+    cout << "Time taken: " << duration.count() << " microseconds\n";
+}
--- laba4/monitor.cpp
+++ laba4/monitor.cpp
@@ -1,14 +1,79 @@
 /*
 Монитор - высокоуровневый механизм синхронизации процессов, 
-обеспечивающий доступ к разделяемым ресурсам.При 
+обеспечивающий доступ к разделяемым ресурсам. При 
 многозадачности, основанной на мониторах, компилятор или 
 интерпретатор прозрачно для программиста вставляет код блокировки-
 разблокировки в оформленные соответствующим образом процедуры,
 избавляя программиста от явного обращения к примитивам синхронизации.
 Монитор состоит из:
-1. набора процедур, взаимодействующих с общим ресурсом
-2. мьютекса
-3. переменных, связанных с этим ресурсом
-4. инварианта, который определяет условия, позволяющие избежать 
-состояние гонки
+1. мьютекса
+2. переменных, связанных с этим ресурсом
+3. флага блокировки
 */
+
+#include <iostream>
+#include <random>
+#include <string>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <atomic>
+#include <condition_variable>
+
+using namespace std;
+
+class Monitor {
+public:
+    void Acquire(){
+        unique_lock<std::mutex> lock(Mutex);
+        cv.wait(lock, [this]() {return !is_locked; }); // ждем освобождения
+        is_locked = true; // забираем ресурс
+    }
+
+    void Release(){
+        lock_guard<std::mutex> lock(Mutex);
+        is_locked = false; // освобождаем ресурс
+        cv.notify_one(); // оповещаем один ожидающий поток
+    }
+private:
+    mutex Mutex; // мьютекс для защиты состояния
+    condition_variable cv; // условная переменная для ожидания
+    bool is_locked; // флаг блокировки
+};
+
+Monitor monitor; // только 1 поток может работать одновременно
+
+void randomChars(int thread_id, int letter_count) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(32, 126);  // ASCII диапазон видимых символов
+    string chars;
+    monitor.Acquire();
+    for (int i = 0; i < letter_count; ++i){
+        char letter = static_cast<char>(dis(gen));
+        chars += letter;
+    }
+    cout << "Thread " << thread_id << ": " << chars << endl;  
+    monitor.Release();
+}
+
+int main(){
+    using namespace std::chrono;
+
+    vector<thread> threads;
+    auto start = high_resolution_clock::now(); // начало таймера
+    
+    for (int i = 0; i < 4; ++i) { // Запуск потоков
+        threads.push_back(thread(randomChars, i, 10));
+    }
+
+    // Ожидание завершения всех потоков
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    auto end = high_resolution_clock::now(); // конец таймера
+    auto duration = duration_cast<microseconds>(end - start);
+    cout << "Time taken: " << duration.count() << " microseconds\n";
+    return 0;
+}
